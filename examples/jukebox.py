fc569dcf62e52944a1e36180b646b41f2a3dd487
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2009-04-05 21:28:14 +0000

    * examples/jukebox.py: 	  Add options for playlist, randomize, size, sink.

--- examples/jukebox.py
+++ examples/jukebox.py
@@ -3,6 +3,7 @@
 
 import sys
 import random
+import optparse
 
 import pickle
 
@@ -21,7 +22,7 @@ _TEMPLATE = gst.PadTemplate('template', gst.PAD_SINK, gst.PAD_ALWAYS,
     gst.caps_from_string('audio/x-raw-int; audio/x-raw-float'))
 
 class Main(object):
-    def __init__(self, loop, tracks, playlist=None):
+    def __init__(self, loop, tracks, options):
         """
         @param tracks: dict of path -> list of mixdata
         @type  tracks: dict of str -> list of L{dad.audio.mixing.MixData}
@@ -33,17 +34,18 @@ class Main(object):
         self._pipeline = gst.Pipeline()
 
         self._identity = gst.element_factory_make('identity')
-        # self._identity.connect('notify::last-message', lambda o, a: sys.stdout.write('%r\n' % self._identity.props.last_message))
+        # self._identity.connect('notify::last-message',
+        #    lambda o, a: sys.stdout.write(
+        #        '%r\n' % self._identity.props.last_message))
         self._identity.props.single_segment = True
 
         ac = gst.element_factory_make('audioconvert')
         queue = gst.element_factory_make('queue')
         
         # parse the sink as a bin, linking to the unconnected pad
-        sink = gst.parse_launch('bin. ( autoaudiosink )')
+        sink = gst.parse_launch('bin. ( %s )' % options.sink)
         # FIXME: changed to find_unlinked_pad in gstreamer 0.10.20
         pad = sink.find_unconnected_pad(gst.PAD_SINK)
-        # sink = gst.parse_launch('bin. ( vorbisenc name=enc ! oggmux ! filesink location=mix.ogg )')
         gpad = gst.GhostPad('ghost', pad)
         sink.add_pad(gpad)
 
@@ -55,10 +57,13 @@ class Main(object):
 
         # pick songs
         files = tracks.keys()
-        if playlist:
-            files = open(playlist).readlines()
+        if options.playlist:
+            files = open(options.playlist).readlines()
 
-        paths = [random.choice(files) for i in range(200)]
+        if options.random:
+            paths = [random.choice(files) for i in range(options.count)]
+        else:
+            paths = files[:options.count]
 
         for path in paths:
             path = path.strip()
@@ -99,18 +104,35 @@ class Main(object):
 def main():
     log.init('DAD_DEBUG')
 
-    if len(sys.argv) < 2:
+    parser = optparse.OptionParser()
+
+    default = 100
+    parser.add_option('-c', '--count',
+        action="store", dest="count",
+        help="how many tracks to play (defaults to %d)" % default,
+        default=default)
+    parser.add_option('-p', '--playlist',
+        action="store", dest="playlist",
+        help="playlist to play from")
+    parser.add_option('-r', '--random',
+        action="store_true", dest="random",
+        help="play tracks in random order")
+    default = 'autoaudiosink'
+    parser.add_option('-s', '--sink',
+        action="store", dest="sink",
+        help="GStreamer audio sink to output to (defaults to %s" % default,
+        default=default)
+
+    opts, args = parser.parse_args(sys.argv[1:])
+
+    if len(args) < 1:
         print 'Please give a tracks pickle path'
 
-    tracks = pickle.load(open(sys.argv[1]))
-    playlist = None
-
-    if len(sys.argv) > 2:
-        playlist = sys.argv[2]
+    tracks = pickle.load(open(args[0]))
 
     loop = gobject.MainLoop()
 
-    main = Main(loop, tracks, playlist)
+    main = Main(loop, tracks, opts)
 
     main.start()
     print 'going into main loop'
