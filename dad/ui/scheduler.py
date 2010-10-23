fda91750e9aad2df4df4a6716b4cb047f9659880
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2010-10-23 22:28:21 +0000

    * dad/ui (added): 	* dad/ui/scheduler.py (added): 	* dad/ui/__init__.py (added): 	  Add a simple GUI.

---
+++ dad/ui/scheduler.py
@@ -0,0 +1,66 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+import os
+
+import gst
+import gobject
+import gtk
+
+(COLUMN_PATH, COLUMN_START, COLUMN_END) = range(3)
+
+class SchedulerUI(gtk.TreeView):
+    def __init__(self):
+        gtk.Widget.__init__(self)
+
+        self._store = gtk.ListStore(
+            gobject.TYPE_STRING,
+            gobject.TYPE_STRING,
+            gobject.TYPE_STRING,
+            )
+
+        self._treeview = self # gtk.TreeView()
+
+        column = gtk.TreeViewColumn('Path', gtk.CellRendererText(),
+                                    text=COLUMN_PATH)
+        self._treeview.append_column(column)
+
+        column = gtk.TreeViewColumn('Start', gtk.CellRendererText(),
+                                    text=COLUMN_START)
+        self._treeview.append_column(column)
+    
+        column = gtk.TreeViewColumn('End', gtk.CellRendererText(),
+                                    text=COLUMN_END)
+        self._treeview.append_column(column)
+    
+    def set_scheduler(self, scheduler):
+        scheduler.connect('scheduled', self._scheduled_cb)
+
+    def _scheduled_cb(self, scheduler, scheduled):
+        self.add_scheduled(scheduled)
+
+    def add_scheduled(self, scheduled):
+        iter = self._store.append()
+        self._store.set(iter,
+            COLUMN_PATH, os.path.basename(scheduled.path),
+            COLUMN_START, gst.TIME_ARGS(scheduled.start),
+            COLUMN_END, gst.TIME_ARGS(scheduled.start + scheduled.duration),
+        )
+        self._treeview.set_model(self._store)
+
+gobject.type_register(SchedulerUI)
+
+def main():
+    window = gtk.Window()
+
+    s = SchedulerUI()
+
+    window.add(s)
+    window.connect('destroy', lambda _: gtk.main_quit())
+
+    window.show_all()
+
+if __name__ == "__main__":
+    main()
+    gtk.main()
+
