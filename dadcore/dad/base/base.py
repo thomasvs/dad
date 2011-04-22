8ae184348b446557ee292c1cb7f9d2f54cab3585
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-04-22 19:41:54 +0000

    * dad/base/base.py: 	  Add viewAdded() to model base class.

--- dadcore/dad/base/base.py
+++ dadcore/dad/base/base.py
@@ -8,16 +8,20 @@ Base classes.
 from dad.extern.log import log
 
 # base class for model
+
+
 class Model(log.Loggable):
 
     logCategory = 'model'
 
     pass
 
+
 class View(log.Loggable):
     """
     I am a base class for views.
     """
+
 
 class SelectorView(View):
     """
@@ -36,15 +40,30 @@ class SelectorView(View):
         """
         pass
 
+
 class Controller(log.Loggable):
+
     def __init__(self, model):
         self._model = model
         self._views = []
 
+    ### base class methods
+
     def addView(self, view):
         self._views.append(view)
+        self.viewAdded(view)
 
     def doViews(self, method, *args, **kwargs):
         for view in self._views:
             m = getattr(view, method)
             m(*args, **kwargs)
+
+    ### subclass methods
+
+    def viewAdded(self, view):
+        """
+        This method is called after a view is added.
+        It can be used to connect to signals on the view.
+        """
+        pass
+
