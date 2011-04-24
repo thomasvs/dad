be47b010bab033102ee1783cbb75eebbae77d9b6
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-04-24 19:34:33 +0000

    * tests/artists.py: 	  Controllers are now moved to dadcore.

--- dadcouch/tests/artists.py
+++ dadcouch/tests/artists.py
@@ -16,6 +16,7 @@ import gtk
 
 from dad.base import base
 from dad.extern.log import log
+from dad.controller import selector
 
 from dadcouch.model import daddb
 from dadcouch.selecter import couch
@@ -23,88 +24,6 @@ from dadcouch.selecter import couch
 from dadcouch.extern.paisley import client
 
 from dadgtk.views import views, scheduler
-
-# move to base class
-class SelectorController(base.Controller):
-    def __init__(self, model):
-        base.Controller.__init__(self, model)
-
-        from twisted.internet import reactor
-        self._reactor = reactor
-
-    def populate(self):
-        self.debug('populate()')
-        self.doViews('throb', True)
-
-        # populate with the iterable we get from the model
-        d = self._model.get()
-
-        def cb(iterable):
-            # iterable can be generator
-            #self.debug('got results: %r' % len(iterable))
-            self.debug('populating')
-
-            # space out add_artist calls from the iterator in blocks of a given
-            # size; this allows the throbber to update
-            def space(iterator, size=1):
-                for i in range(size):
-                    try:
-                        item = iterator.next()
-                    except StopIteration:
-                        self.doViews('throb', False)
-                        self.debug('filled view')
-                        return
-
-                    self.addItem(item)
-                self._reactor.callLater(0, space, iterator, size)
-
-            self._reactor.callLater(0, space, iter(iterable), size=11)
-            self.debug('populated')
-        d.addCallback(cb)
-
-        def eb(failure):
-            self.warningFailure(failure)
-            self.doViews('error', "failed to populate",
-                "%r: %r" % (failure, failure.value.args))
-        d.addErrback(eb)
-
-        return d
-
-    def addItem(self, item):
-        raise NotImplementedError, \
-            "implement addItem using self.doViews('add_row', ...)"
-
-class ArtistSelectorController(SelectorController):
-
-    logCategory = 'artistSC'
-
-    def addItem(self, item):
-        # tracks can be 0 for Various Artists for example, which own albums
-        # but no tracks
-        if item.tracks == 0:
-            return
-
-        self.doViews('add_row', item.id, "%s (%d)" % (item.name, item.tracks),
-            item.sortname, item.tracks)
-
-class AlbumSelectorController(SelectorController):
-
-    logCategory = 'albumSC'
-
-    def addItem(self, item):
-        # add an album and the count of tracks on it
-        self.doViews('add_row', item.id, "%s (%d)" % (item.name, item.tracks),
-            item.sortname, item.tracks)
-
-class TrackSelectorController(SelectorController):
-
-    logCategory = 'trackSC'
-
-    def addItem(self, item):
-        # add a track
-        self.doViews('add_item', item, [a.name for a in item.artists],
-            "%s" % item.name, None, None, None)
-
 
 def main():
     from twisted.internet import reactor
@@ -140,19 +59,19 @@ def main():
 
     artistView = views.ArtistSelectorView()
     artistModel = daddb.ArtistSelectorModel(db)
-    artistController = ArtistSelectorController(artistModel)
+    artistController = selector.ArtistSelectorController(artistModel)
     artistController.addView(artistView)
     hbox.pack_start(artistView)
 
     albumView = views.AlbumSelectorView()
     albumModel = daddb.AlbumSelectorModel(db)
-    albumController = AlbumSelectorController(albumModel)
+    albumController = selector.AlbumSelectorController(albumModel)
     albumController.addView(albumView)
     hbox.pack_start(albumView)
 
     trackView = scheduler.TracksUI()
     trackModel = daddb.TrackSelectorModel(db)
-    trackController = TrackSelectorController(trackModel)
+    trackController = selector.TrackSelectorController(trackModel)
     trackController.addView(trackView)
 
     vbox.add(trackView)
