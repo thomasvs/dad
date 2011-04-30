921331c283bb5dcad71ab459c39fc58dbbd0ae92
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-04-30 17:34:07 +0000

    * python/dadcouch/models (added): 	* python/dadcouch/models/__init__.py (added): 	* python/dadcouch/models/app.py (added): 	  Add a CouchAppModel.

---
+++ dadcouch/python/dadcouch/models/app.py
@@ -0,0 +1,21 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+from twisted.python import reflect
+
+from dad.base import app
+
+class CouchAppModel(app.AppModel):
+
+    def __init__(self, daddb):
+        self._daddb = daddb
+
+    def getModel(self, what):
+        name = 'dadcouch.models.%s.%sModel' % (what.lower(), what)
+        # FIXME: move
+        if what == 'Track':
+            name = 'dadcouch.model.daddb.TrackModel'
+        model = reflect.namedAny(name)(self._daddb)
+        return model
+
+        
