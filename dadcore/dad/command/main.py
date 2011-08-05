2cd5695cd7515e21ef4e3dc01b781c52db1dec32
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-08-05 18:25:13 +0000

    * bin/dad.in (svn:executable): 	* bin/dad (svn:special): 	* dad/common/logcommand.py (added): 	* dad/command (added): 	* dad/command/__init__.py (added): 	* dad/command/main.py (added): 	* bin (added): 	* bin/dad (added): 	* bin/dad.in (added): 	  Add a dad command.

---
+++ dadcore/dad/command/main.py
@@ -0,0 +1,60 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+import sys
+
+from dad.common import log
+from dad.common import logcommand
+
+from dad.extern.command import command
+
+def main(argv):
+    c = Dad()
+    try:
+        ret = c.parse(argv)
+    except SystemError, e:
+        sys.stderr.write('rip: error: %s\n' % e.args)
+        return 255
+    except ImportError, e:
+        # FIXME: decide how to handle
+        raise
+        # deps.handleImportError(e)
+    except command.CommandError, e:
+        sys.stderr.write('rip: error: %s\n' % e.output)
+        return e.status
+
+    if ret is None:
+        return 0
+
+    return ret
+
+class Dad(logcommand.LogCommand):
+    usage = "%prog %command"
+    description = """DAD is a digital audio database.
+
+DAD gives you a tree of subcommands to work with.
+You can get help on subcommands by using the -h option to the subcommand.
+"""
+
+    subCommandClasses = []
+
+    def addOptions(self):
+        # FIXME: is this the right place ?
+        log.init()
+        # from dad.configure import configure
+        log.debug("dad", "This is dad version %s (%s)", "0.0.0", "0")
+        #    configure.version, configure.revision)
+
+        self.parser.add_option('-v', '--version',
+                          action="store_true", dest="version",
+                          help="show version information")
+
+    def handleOptions(self, options):
+        if options.version:
+            from dad.configure import configure
+            print "dad %s" % configure.version
+            sys.exit(0)
+
+    def parse(self, argv):
+        log.debug("dad", "dad %s" % " ".join(argv))
+        logcommand.LogCommand.parse(self, argv)
