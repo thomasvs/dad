d850eca5c61afe4b707803c7ceb03d67403abe9d
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-08-08 21:12:15 +0000

    * python/dadcouch/command/lookup.py: 	  Handle lookup of track, works on tracks not in database.

--- dadcouch/python/dadcouch/command/lookup.py
+++ dadcouch/python/dadcouch/command/lookup.py
@@ -5,10 +5,16 @@
 
 import os
 
-from dad.audio import common
+from twisted.internet import reactor
+from twisted.internet import defer
+from twisted.web import error
+
 from dad.common import logcommand
 
+from dadcouch.model import daddb
 from dadcouch.selecter import couch
+
+from dadcouch.extern.paisley import client
 
 
 class Lookup(logcommand.LogCommand):
@@ -18,14 +24,39 @@ class Lookup(logcommand.LogCommand):
         self.parser.add_options(couch.couchdb_option_list)
     
     def do(self, args):
+        self._db = client.CouchDB(self.options.host, int(self.options.port))
+        self._daddb = daddb.DADDB(self._db, self.options.database)
+
+        d = self._doCb(args)
+        d.addCallback(lambda _: reactor.stop)
+
+        reactor.run()
+
+    @defer.inlineCallbacks
+    def _doCb(self, args):
         for path in args:
             path = path.decode('utf-8')
             if not os.path.exists(path):
                 self.stderr.write('Could not find %s\n' % path.encode('utf-8'))
                 continue
-
-
+        
             self.stdout.write('%s\n' % path)
+            try:
+                ret = yield self._daddb.getTrackByHostPath(u'localhost', path)
+            except error.Error, e:
+                if e.status == 404:
+                    self.stderr.write('Database or view does not exist.\n')
+                    reactor.stop()
+                    defer.returnValue(3)
+                    return
+
+            ret = list(ret)
+            if len(ret) == 0:
+                self.stdout.write('Not in database.\n')
+            else:
+                self.stdout.write('In database in %d tracks.\n' % len(ret))
+
+        reactor.stop()
 
 # called by main command code before instantiating the class
 def plugin(dadCommandClass):
