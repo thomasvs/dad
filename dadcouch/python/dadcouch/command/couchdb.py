401d90a7f1aa2fddfaf096fdf8f7bfea2f2b00bd
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-08-08 21:52:42 +0000

    * python/dadcouch/command/lookup.py (deleted): 	* python/dadcouch/command/couchdb.py: 	  Move command into couchdb.

--- dadcouch/python/dadcouch/command/couchdb.py
+++ dadcouch/python/dadcouch/command/couchdb.py
@@ -80,8 +80,59 @@ class Add(logcommand.LogCommand):
 
             self.stdout.write('Stored in database under id %r\n' %
                  stored['id'])
-            
+
+
+class Lookup(logcommand.LogCommand):
+    description = """Look up audio files in the database."""
+
+    def addOptions(self):
+        self.parser.add_options(scouch.couchdb_option_list)
+    
+    def do(self, args):
+        self._db = client.CouchDB(self.options.host, int(self.options.port))
+        self._daddb = daddb.DADDB(self._db, self.options.database)
+
+        def later():
+            d = self._doCb(args)
+            d.addCallback(lambda _: reactor.stop())
+
+        reactor.callLater(0, later)
+
+        reactor.run()
+
+    @defer.inlineCallbacks
+    def _doCb(self, args):
+        if not args:
+            self.stderr.write('Please give paths to look up.\n')
+            defer.returnValue(3)
+            return
+
+        import socket
+        hostname = unicode(socket.gethostname())
+
+        for path in args:
+            path = path.decode('utf-8')
+            if not os.path.exists(path):
+                self.stderr.write('Could not find %s\n' % path.encode('utf-8'))
+                continue
+        
+            self.stdout.write('%s\n' % path)
+            try:
+                ret = yield self._daddb.getTrackByHostPath(hostname, path)
+            except error.Error, e:
+                if e.status == 404:
+                    self.stderr.write('Database or view does not exist.\n')
+                    reactor.stop()
+                    defer.returnValue(3)
+                    return
+
+            ret = list(ret)
+            if len(ret) == 0:
+                self.stdout.write('Not in database.\n')
+            else:
+                self.stdout.write('In database in %d tracks.\n' % len(ret))
+ 
 class CouchDB(logcommand.LogCommand):
     summary = """Interact with CouchDB backend."""
 
-    subCommandClasses = [Add, lookup.Lookup]
+    subCommandClasses = [Add, Lookup]
