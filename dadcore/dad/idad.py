60645857db2a3a67b76dcbb3e39b184d5117d076
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-08-08 21:11:52 +0000

    * dad/idad.py: 	  Update interface for database provider.

--- dadcore/dad/idad.py
+++ dadcore/dad/idad.py
@@ -3,12 +3,20 @@
 
 from zope import interface
 
+"""
+Interfaces for functionality provided by add-on modules.
+"""
+
 class ICommand(interface.Interface):
     def addCommands(commandClass):
         pass
 
-class IDatabaseProvider(interface.Interface):
-    pass
+class IDatabase(interface.Interface):
+    def lookupPath(path):
+        """
+        @type path: unicode
+        """
+        pass
 
 class IAnalyzer(interface.Interface):
     pass
