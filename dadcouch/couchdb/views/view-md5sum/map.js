f25d6c184cfc748cb3fe749f8ce6adf7a1557057
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-08-09 18:08:13 +0000

    * couchdb/views/view-md5sum/map.js: 	  For each document, emit each md5sum only once, to avoid duplicate 	  merging.

--- dadcouch/couchdb/views/view-md5sum/map.js
+++ dadcouch/couchdb/views/view-md5sum/map.js
@@ -1,10 +1,17 @@
+// For each track, emit all different md5sums as key
+// and the track id as value
 function(doc) {
+    var seen = {};
+
     if (doc.type == 'track') {
         doc.fragments.forEach(
             function(fragment) {
                 fragment.files.forEach(
                     function(file) {
-                        emit(file.md5sum, doc._id);
+                        if (!(file.md5sum in seen)) {
+                            emit(file.md5sum, doc._id);
+                            seen[file.md5sum] = 1;
+                        }
                     }
                 )
             }
