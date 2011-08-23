d662a67a22fc9d0b4d96152e74bd484c11200852
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-08-23 20:09:09 +0000

    * dad/model/artist.py (added): 	  Add ArtistModel.

---
+++ dadcore/dad/model/artist.py
@@ -0,0 +1,21 @@
+# -*- Mode: Python; test_case_name: dad.test.test_model_track -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+from dad.base import base
+
+class ArtistModel(base.Model):
+    """
+    I am a model for an artist.
+
+    My controller is L{dad.controller.track.ArtistController}
+    """
+    def getName(self):
+        """
+        Return the name of the artist, suitable for display.
+
+        @rtype: C{unicode}
+        """
+        raise NotImplementedError
+
+    def __repr__(self):
+        return '<Artist %r>' % (self.getName(), )
