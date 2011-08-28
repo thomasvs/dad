afa9e817b9e2ee1f900d19a65633d17d77b86dd8
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-08-28 14:54:39 +0000

    * couchdb/views/view-scores-by-subject/map.js: 	  Artists can be scored too.

--- dadcouch/couchdb/views/view-scores-by-subject/map.js
+++ dadcouch/couchdb/views/view-scores-by-subject/map.js
@@ -1,6 +1,7 @@
 // for each score, emit subject of score
 function(doc) {
-    if (doc.type == 'track' && doc.scores) {
+
+    if (doc.scores) {
         doc.scores.forEach(
             function(score) {
     	        emit(doc._id, [score.user, score.category, score.score]);
