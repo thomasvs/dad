457899c4d4651cd04678a80000b8e329db3a06c8
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-09-03 11:19:19 +0200

    remove imports

--- dadcouch/python/dadcouch/test/test_model_track.py
+++ dadcouch/python/dadcouch/test/test_model_track.py
@@ -1,25 +1,12 @@
 # -*- Mode: Python; test_case_name: dadcouch.test.test_model_track -*-
 # vi:si:et:sw=4:sts=4:ts=4
 
-import os
-import commands
-
 from twisted.internet import defer
 
-from dad.logic import database
-
-from dad.test import test_database_memory
-
-from dad.plugins import pdadcouch
-
-from dadcouch.extern.paisley.test import test_util
-
-from dadcouch.model import daddb, couch
-from dadcouch.model import track as mtrack, artist
+from dadcouch.model import couch
+from dadcouch.model import track as mtrack
 
 from dadcouch.test import test_model_daddb
-
-
 
 
 class TrackModelTestCase(test_model_daddb.DADDBTestCase):
