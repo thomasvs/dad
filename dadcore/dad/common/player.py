32b1fcc901721ce558125e77f823c6fb9e60bd62
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-09-15 16:33:36 +0200

    remove imports

--- dadcore/dad/common/player.py
+++ dadcore/dad/common/player.py
@@ -18,11 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import os
-import sys
 import time
-
-from twisted.internet import defer
 
 from dad.extern.log import log
 
