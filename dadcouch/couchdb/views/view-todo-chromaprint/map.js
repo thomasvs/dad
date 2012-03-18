b437a239323c5205be3fb2be61596fb988be59f2
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-03-18 23:02:09 +0100

    fix todo for chromaprint

--- dadcouch/couchdb/views/view-todo-chromaprint/map.js
+++ dadcouch/couchdb/views/view-todo-chromaprint/map.js
@@ -2,7 +2,7 @@ function(doc) {
     if (doc.type == 'track') {
         doc.fragments.forEach(
             function(fragment) {
-                if (!fragment.chroma) {
+                if (!fragment.chroma || !fragment.chroma.chromaprint) {
                     fragment.files.forEach(
                         function(file) {
                             emit([file.host, file.path], 1);
