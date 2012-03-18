17f0dccabadb87dac4d18680080874fc6ec7d6e7
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-03-18 22:24:47 +0100

    make unit clear with S and Ms

--- dadws/dadws/www/player.js
+++ dadws/dadws/www/player.js
@@ -20,40 +20,41 @@ $(document).ready(function() {
         console.log('ws: message: ' + evt.data);
         var message = jQuery.parseJSON(evt.data);
         if (message.command == 'load') {
+            // load a new track
             console.log('ws: load: id ' + message.id + ': ' + message.artists + ' - ' + message.title);
             $('#audio > tbody:last').append(
-                        '<tr><td>' + message.id + '</td>' +
+                        '<tr id="tr-' + message.id + '"><td>' + message.id + '</td>' +
                     '<td>' + message.artists + '</td>' +
                     '<td>' + message.title + '</td>' +
                     '<td>' + new Date(message.when * 1000) + '</td>' +
-                    '<td><audio id="' + message.id + '" src="' + message.uri + '" controls="controls"/></td></tr>');
-            var audio = $('#' + message.id).get(0);
+                    '<td><audio id="audio-' + message.id + '" src="' + message.uri + '" controls="controls"/></td></tr>');
+            var audio = $('#audio-' + message.id).get(0);
             document.audios[message.id] = audio;
 
-            when = message.when * 1000;
-            var remaining = when - new Date().getTime();
+            whenMs = message.when * 1000;
+            var remainingMs = whenMs - new Date().getTime();
             at.at(function() {
                 var a = document.audios[message.id];
                 console.log('at.at: id ' + message.id + ': play after ' + (new Date().getTime() - document.start) + ' sec since document.start');
 
-                offset = message.offset;
-                console.log('at.at: id ' + message.id + ': start track at ' + offset + ' sec');
-                if (remaining < 0) {
+                offsetS = message.offset;
+                console.log('at.at: id ' + message.id + ': start track at ' + offsetS + ' sec');
+                if (remainingMs < 0) {
                     // FIXME: handle forwarding passed the end, which plays from 0
-                    console.log('at.at: id ' + message.id + ': forward track by ' + (-remaining) + ' msec');
-                    offset += -remaining / 1000.0;
+                    console.log('at.at: id ' + message.id + ': forward track by ' + (-remainingMs) + ' msec');
+                    offsetS += -remainingMs / 1000.0;
                 }
 
                 // we can only seek after we have metadata
                 function loadSeek(event) {
-                    console.log('loadSeek: id ' + message.id + ': set currentTime to ' + offset);
-                    a.currentTime = offset;
+                    console.log('loadSeek: id ' + message.id + ': set currentTime to ' + offsetS);
+                    a.currentTime = offsetS;
                 }
                 audio.addEventListener('loadedmetadata', loadSeek, false);
                 a.play();
 
+        // get and show images for this artist
 	    echonest.artist(message.artists[0]).images( function(imageCollection) {
-		console.log(imageCollection);
 		var images = [];
 		for (i in imageCollection.data.images) {
 		    images.push({
@@ -71,9 +72,11 @@ $(document).ready(function() {
 
 	    });
 
-            }, when);
-            console.log('load: id ' + message.id + ': scheduled play at ' + when + ' epoch sec in ' + remaining + ' msec');
+            }, whenMs);
+            console.log('load: id ' + message.id + ': scheduled play at ' + whenMs + ' epoch msec in ' + remainingMs + ' msec');
         }
+
+        // FIXME: currently not used
         if (message.command == 'play') {
                 console.log('Schedule play of ' + message.id +
                         ' at' + (message.when * 1000) + ' sec');
