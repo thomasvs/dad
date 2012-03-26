1570715635e93416bd6921f75845fbe2317830aa
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-03-26 13:22:00 +0200

    remove duplicated code

--- dadcore/dad/model/scorable.py
+++ dadcore/dad/model/scorable.py
@@ -5,26 +5,10 @@
 Base classes for model/view/controller.
 """
 
-from dad.extern.log import log
-
-# base class for model
+from dad.base import base
 
 
-class Model(object):
-    """
-    I am a base class for models.
-    I hold data coming from a database that can be presented in a view through
-    the controller.
-
-    I can notify views of changes through the controller.
-
-    @type controller: L{Controller}
-    """
-
-    controller = None
-
-
-class ScorableModel(Model):
+class ScorableModel(base.Model):
     """
     I am a base class for models that can be given scores.
     """
@@ -74,19 +58,8 @@ class ScorableModel(Model):
         raise NotImplementedError
 
 
-class View(log.Loggable):
-    """
-    I am a base class for views.
-
-    @ivar controller: L{Controller}
-    """
-
-    controller = None
-
-    # FIXME: error ?
-
 # FIXME: decide on snake case vs camelCase
-class SelectorView(View):
+class SelectorView(base.View):
     """
     I am a base class for selector widgets.
     """
@@ -109,85 +82,5 @@ class SelectorView(View):
         Start or stop throbbing the selector to indicate activity.
 
         @param active: whether to throb
-        """
-        pass
-
-
-class Controller(log.Loggable):
-    """
-    I am a base class for controllers.
-    I interact with one model and one or more views.
-
-    Controllers can be nested to form an application hierarchy.
-
-    Controllers have a reference to their model.
-
-    Controllers can invoke methods on all views.
-
-    @type parent: L{Controller}
-    """
-
-    parent = None
-
-    # FIXME: self._model is used in subclasses
-
-    def __init__(self, model):
-        """
-        @type  model: L{Model}
-        """
-        self._model = model
-        model.controller = self
-
-        self._views = []
-        self._controllers = []
-
-    ### base class methods
-
-    def addView(self, view):
-        """
-        @type  view: L{View}
-        """
-        self._views.append(view)
-        view.controller = self
-        self.viewAdded(view)
-
-    def doViews(self, method, *args, **kwargs):
-        """
-        Call the given method on all my views, with the given args and kwargs.
-        """
-        for view in self._views:
-            m = getattr(view, method)
-            m(*args, **kwargs)
-
-    def add(self, controller):
-        """
-        Add a child controller to me.
-        Takes parentship over the controller.
-
-        @type controller: subclass of L{Controller}
-        """
-        self._controllers.append(controller)
-        controller.parent = self
-
-    def getRoot(self):
-        """
-        Get the root controller for this controller.
-
-        @rtype: subclass of L{Controller}
-        """
-        parent = self.parent
-        while parent.parent:
-            parent = parent.parent
-
-        return parent
-
-    ### subclass methods
-
-    def viewAdded(self, view):
-        """
-        This method is called after a view is added.
-        It can be used to connect to signals on the view.
-
-        @type  view: L{View}
         """
         pass
