c628e1de988540e28951281944eb10d7a0c2a7e9
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-04-11 21:47:12 +0200

    implement selection

--- dadcouch/python/dadcouch/model/selection.py
+++ dadcouch/python/dadcouch/model/selection.py
@@ -6,7 +6,7 @@ import time
 from twisted.internet import defer
 
 from dadcouch.database import mappings
-from dadcouch.model import track as ctrack, base
+from dadcouch.model import base
 from dad.model import selection
 
 # FIXME: remove track attribute
@@ -42,24 +42,17 @@ class CouchSelectionModel(selection.Selection, base.CouchBaseDocModel):
             len(trackList), time.time() - last[0])
         last[0] = time.time()
 
-        # FIXME: this part unfinished; find a good view to use so we can
-        # get all tracks by an artist
+        artists = []
         ret = []
+
+        # FIXME: reduce this somewhere
         for trackRow in trackList:
-            print trackRow
-            # FIXME: find a better way to convert to a track; use a model
-            track = mappings.Track()
-            d = {
-                '_id': trackRow.id,
-                'name': trackRow.name,
-                'artists': trackRow.artists,
-                'albums': trackRow.albums,
-            }
-            track.fromDict(d)
-            tm = ctrack.CouchTrackModel(self.database)
-            # FIXME: remove track attribute
-            tm.track = track
-            tm.document = track
-            ret.append(tm)
+            mid = yield trackRow.getMid()
+            if mid in artists:
+                continue
+            artists.append(mid)
+
+            tracks = yield trackRow.getTracks()
+            ret.extend(tracks)
 
         defer.returnValue(ret)
