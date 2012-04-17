32e3cafc1b7a313a06e3846c9bd2783507fca0b9
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-04-17 21:50:31 +0200

    fix test line

--- dadcore/dad/memorydb/model/base.py
+++ dadcore/dad/memorydb/model/base.py
@@ -1,4 +1,4 @@
-# -*- Mode: Python; test-case-name: dad.test.test_database_memory -*-
+# -*- Mode: Python; test-case-name: dad.test.test_memorydb_memory -*-
 # vi:si:et:sw=4:sts=4:ts=4
 
 from dad.base import base
