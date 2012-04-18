fd86625227df5c9a9c095cb4352600ee99d4358e
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-04-18 09:55:38 +0200

    style fixes

--- dadcouch/couchdb/views/view-mbtrackid/map.js
+++ dadcouch/couchdb/views/view-mbtrackid/map.js
@@ -1,22 +1,27 @@
-// For each track, emit all different md5sums as key
-// and the track id as value
+// vi:si:et:sw=4:sts=4:ts=4
+
+// For each track, emit
+// key: mb track id
+// value: doc id
+
+
 function(doc) {
     var seen = {};
 
     if (doc.type == 'track') {
         doc.fragments.forEach(
-            function(fragment) {
-                fragment.files.forEach(
-                    function(file) {
-                        if (file.metadata && file.metadata.mb_track_id) {
-                            if (!(file.metadata.mb_track_id in seen)) {
-                            	emit(file.metadata.mb_track_id, doc._id);
-				seen[file.metadata.mb_track_id] = 1;
-			    }
-                        }
+
+        function(fragment) {
+            fragment.files.forEach(
+
+            function(file) {
+                if (file.metadata && file.metadata.mb_track_id) {
+                    if (!(file.metadata.mb_track_id in seen)) {
+                        emit(file.metadata.mb_track_id, doc._id);
+                        seen[file.metadata.mb_track_id] = 1;
                     }
-                )
-            }
-        )
+                }
+            });
+        });
     }
 }
